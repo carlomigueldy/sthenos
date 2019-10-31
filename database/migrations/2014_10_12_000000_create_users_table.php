3f8c950f6744ce55132841ab8ecf6cf840307469
Carlo Miguel Dy <carlomigueldy@gmail.com>
2019-10-31 18:38:54 +0800

    added defautl value for role

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->tinyIncrements('id');
-            $table->unsignedTinyInteger('role_id');
+            $table->unsignedTinyInteger('role_id')->default(1);
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
