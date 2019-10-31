f42d503886414d747fe89b1e4b954e8abf35fbfb
Carlo Miguel Dy <carlomigueldy@gmail.com>
2019-10-31 17:39:59 +0800

    foreign key quick bug fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,7 +24,7 @@ class CreateUsersTable extends Migration
             $table->timestamps();
         });
 
-        Schema::table('roles', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->foreign('role_id')->references('id')->on('roles');
         });
     }
