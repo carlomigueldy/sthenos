7c3e3b271ac82f63f3ae562861879e00f7e518c5
Carlo Miguel Dy <carlomigueldy@gmail.com>
2019-10-31 16:36:07 +0800

    encrypt the password

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -35,7 +35,7 @@ class AuthController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => $request->password,
+            'password' => bcrypt($request->password),
         ]);
 
         return response()->json([
