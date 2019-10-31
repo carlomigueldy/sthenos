d48d9cfa8dfac18d20a3cdc774dbae89f2402a29
Carlo Miguel Dy <carlomigueldy@gmail.com>
2019-10-31 17:38:24 +0800

    added table seeder

--- app/Role.php
+++ app/Role.php
@@ -7,7 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     protected $fillable = [
-        'name'
+        'id',
+        'name',
     ];
 
     public $timestamps = false;
---
+++ database/seeds/RoleSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Role;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create([
+            'id' => 1337,
+            'name' => 'Owner',
+        ]);
+    }
+}
