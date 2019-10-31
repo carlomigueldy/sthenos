dcce9aad078216e8d6444b88258fbb864fe7d451
Carlo Miguel Dy <carlomigueldy@gmail.com>
2019-10-31 16:30:49 +0800

    AuthController was added and API routes were defined

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,16 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('auth')->group(function () {
+    Route::post('login', 'AuthController@login');
+
+    /**
+     * Only the authenticated user can 
+     * access these defined routes.
+     */
+    Route::middleware('auth:api')->group(function() {
+        Route::post('logout', 'AuthController@logout');
+        Route::post('refresh', 'AuthController@refresh');
+        Route::post('me', 'AuthController@me');
+    });
 });
